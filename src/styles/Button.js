d89c7155ac635d39ed4842880e2ae86f8fa65428
seongmin <seongm22n@gmail.com>
2023-09-13 07:28:04 +0900

    fix: props명이 어떤것을 의미하는지 수정

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-	background-color: ${(props) => (props.active ? '#9b634c' : '#bfa398')};
+	background-color: ${(barClick) => (barClick.active ? '#9b634c' : '#bfa398')};
 	color: white;
 	padding: 8px 16px;
 	border-radius: 20px;
 	font-family: 'Nanum Pen Script', cursive;
-  font-size: 20px;
+	font-size: 20px;
 
 	&:hover {
 		background-color: #9b634c;
